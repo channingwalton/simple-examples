5e6d146cfe1c92313e5c122611740a362db9df22
Channing Walton <channingwalton@mac.com>
2014-07-27 21:04:27 +0100

    reformatted and brought into line with the blog

--- src/main/scala/io/underscore/examples/readerwriter/ReaderWriterDatabase.scala
+++ src/main/scala/io/underscore/examples/readerwriter/ReaderWriterDatabase.scala
@@ -3,12 +3,7 @@ package io.underscore.examples.readerwriter
 import scalaz._
 
 /**
- * The problem is working with a database.
- * Operations should run in a transaction/connection/context
- * Errors need to be handled without exceptions
- * Side effects like post commits should be supported
- *
- * This is the usual thing:
+ * See http://underscoreconsulting.com/blog/posts/2014/07/08/readerwriterstate.html
  */
 object TheProblem {
 
@@ -45,36 +40,18 @@ object TheProblem {
   }
 }
 
-/**
- * Problems seen in a real codebase during refactoring to the final solution -
- * dev must remember to 'run' to get transaction - no compile time enforcement
- * - forgotten very often
- * - or having no idea whats going on multi-level transactions end up being constructed
- * post commits
- * - can't be bothered
- * - need global or thread-local objects to add success/fail
- * - harder to test
- * no nice error handling
- * - often just ignored
- * Wasteful
- * - typical pattern is to wrap every web request, or queue end point in a transaction when it may not be needed
- *
- * Step 1:
- * Solve the 'running in a transaction problem' with a Reader
- */
-
-
 object ReaderToTheRescue {
 
   import scalaz.Reader
 
   type Key = String
 
-  // The required transaction/connection/context for some database action
   trait Transaction
 
-  // Work represents a unit of work to do against the Database
-  // It is a type alias for a scalaz.Reader, wrapping a Transaction => A
+  /* Work represents a unit of work to do against the Database
+   * It is a type alias for a scalaz.Reader, which wraps
+   * a Transaction => A
+   */
   type Work[+A] = Reader[Transaction, A]
 
   object Database {
@@ -96,10 +73,12 @@ object ReaderToTheRescue {
     def commit() = {}
     def rollback() = {}
 
-    // lift operations into Work - note both of these do nothing here
-    def put[A](key: Key, a: A): Work[Unit] = Reader(Transaction => {})
+    // lift operations into Work - they do nothing here
+    def put[A](key: Key, a: A): Work[Unit] =
+      Reader(Transaction => {})
 
-    def find[A](key: Key): Work[Option[A]] = Reader(Transaction => None)
+    def find[A](key: Key): Work[Option[A]] =
+      Reader(Transaction => None)
   }
 
   // the program
@@ -113,18 +92,6 @@ object ReaderToTheRescue {
   val result: Option[String] = Database.run(work)
 }
 
-/**
- * Observations:
- * - everything now in for-comprehensions rather than imperative
- * - Reader is a function, nothing happens until Database.run
- * - Can no longer operate on the Database without a Transaction
- *
- * Step 2:
- * solve the post commits using a Writer, and the errors problem with
- * scalaz's either type, \/.
- */
-
-
 object ReaderWriterForPostCommits {
 
   import scalaz.Scalaz._
@@ -137,20 +104,28 @@ object ReaderWriterForPostCommits {
   // A class to hold the post commit function
   case class PostCommit(f: () => Unit)
 
-  // Work represents some work to do on the Database
-  // It is a Reader that takes a Transaction and returns a result
-  // It is a Writer that will record post commit actions in a List
-  // It is also a State which is ignored here
-  // ReaderWriterState's type args are: the Reader type, Writer type, State type and A
-  type Work[+A] = ReaderWriterState[Transaction, List[PostCommit], Unit, A]
+  /* Work represents some work to do on the Database
+   * It is a Reader that takes a Transaction and returns a result
+   * It is a Writer that records post commit actions in a List
+   * It is also a State which is ignored here
+   * ReaderWriterState's type args are:
+   *   the Reader type, Writer type, State type and A
+   */
+  type Work[+A] =
+    ReaderWriterState[Transaction, List[PostCommit], Unit, A]
 
   // helper to create Work for some Transaction => T
-  def work[T](f: Transaction => T): Work[T]
-  = ReaderWriterState { (trans, ignored) => (Nil, f(trans), ()) }
+  def work[T](f: Transaction => T): Work[T] =
+    ReaderWriterState {
+      (trans, ignored) => (Nil, f(trans), ())
+    }
 
-  // helper to create Work for a post commit, which is added to the written value
-  def postCommit(f: () => Unit): Work[Unit]
-  = ReaderWriterState { (trans, ignored) => (List(PostCommit(f)), (), ()) }
+  // helper to create Work for a post commit,
+  // PostCommits are added to the written value
+  def postCommit(f: () => Unit): Work[Unit] =
+    ReaderWriterState {
+      (trans, ignored) => (List(PostCommit(f)), (), ())
+    }
 
   object Database {
 
@@ -159,7 +134,9 @@ object ReaderWriterForPostCommits {
     // a convenient method to drop the state part of the result
     // and also could be used in tests to check post commits
     def runWork[T](work: Work[T]): (List[PostCommit], T) = {
-      val (postCommits, result, ignoredState) = work.run(MyTransaction, ())
+      val results = work.run(MyTransaction, ())
+      val (postCommits, result, ignoredState) = results
+
       (postCommits, result)
     }
 
